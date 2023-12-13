054ba46021a8b1d80be70cb39c13d69ea53196bb
RIFKI <125611993+RifkiAhmed@users.noreply.github.com>
2023-12-13 19:14:36 +0100

    Check if the email of the current user matches the value of the ADMIN_EMAIL env. variable

--- modules/redirect_user.py
+++ modules/redirect_user.py
@@ -4,19 +4,19 @@ def redirect_user(currentuser):
     """Redirects users based on their roles.
 
     Args:
-    - user: Object representing the current user.
+    - currentuser: Object representing the current user.
 
     Returns:
     - Redirects to different views based on user roles:
-      - If user is 'Admin' and (user or nurse) exists:
-        - If user's email is not the admin username, redirects to user's home.
+      - If currentuser is 'Admin' and (user or nurse) exists:
+        - If currentuser's email is not the admin email, redirects to user's home.
         - If nurse exists, redirects to nurse's home.
-      - If user is 'User' and (user or nurse) exists:
-        - If user's email is the admin username, redirects to admin's home.
+      - If currentuser is 'User' and (user or nurse) exists:
+        - If currentuser's email is the admin email, redirects to admin's home.
         - If nurse exists, redirects to nurse's home.
-      - If conditions do not match, redirects based on user's email:
-        - If user's email is the admin username, redirects to admin's home.
-        - If user's email is not the admin username, redirects to user's home.
+      - If conditions do not match, redirects based on currentuser's email:
+        - If currentuser's email is the admin email, redirects to admin's home.
+        - If currentuser's email is not the admin email, redirects to user's home.
 
     If no redirection matches the conditions, returns None.
     """
@@ -28,18 +28,18 @@ def redirect_user(currentuser):
     user = storage.get_by_id(User, current_user.id)
     nurse = storage.get_by_id(Nurse, current_user.id)
     if currentuser == 'Admin' and (user or nurse):
-        if user and user.email != os.getenv('ADMIN_USERNAME'):
+        if user and user.email != os.getenv('ADMIN_EMAIL'):
             return redirect(url_for('app_views.user_home'))
         elif nurse:
             return redirect(url_for('app_views.nurse_home'))
     elif currentuser == 'User' and (user or nurse):
-        if user and user.email == os.getenv('ADMIN_USERNAME'):
+        if user and user.email == os.getenv('ADMIN_EMAIL'):
             return redirect(url_for('app_views.admin_home'))
         elif nurse:
             return redirect(url_for('app_views.nurse_home'))
     else:
-        if user and user.email == os.getenv('ADMIN_USERNAME'):
+        if user and user.email == os.getenv('ADMIN_EMAIL'):
             return redirect(url_for('app_views.admin_home'))
-        elif user and user.email != os.getenv('ADMIN_USERNAME'):
+        elif user and user.email != os.getenv('ADMIN_EMAIL'):
             return redirect(url_for('app_views.user_home'))
     return None
